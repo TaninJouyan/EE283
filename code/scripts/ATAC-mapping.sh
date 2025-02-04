0f48e80687c8b18a08d17a677e637c92a5ff407d
TaninJouyan <taninjouyan@gmail.com>
2025-02-03 21:26:57 -0800

    updated script

--- code/scripts/ATAC-mapping.sh
+++ code/scripts/ATAC-mapping.sh
@@ -2,19 +2,20 @@
 #SBATCH --job-name=map-atac    
 #SBATCH -A class-ecoevo283    
 #SBATCH -p standard       
-#SBATCH --cpus-per-task=4  
-#SBATCH --error=map_%J.err
-#SBATCH --output=map_%J.out
+#SBATCH --cpus-per-task=10
+#SBATCH --error=map-atac_%A.err
+#SBATCH --output=map-atac_%A.out
 #SBATCH --mem-per-cpu=3G    
 #SBATCH --time=1-00:00:00   
 #SBATCH --array=1-12
 
-input="data/raw/ATACseq/"
-output="data/processed/ATACseq/bams/"
-ref="data/ref/dm6.fasta"
+input="data/raw/ATACseq"
+output="data/processed/ATACseq/bams"
+ref="data/ref"
 
-cd ${input}
-ls *R1.fq.gz | sed 's/_R1.fq.gz//' >~/ee283/prefixes-atac.txt
+cd ${input}/
+ls *R1.fq.gz | sed 's/_R1.fq.gz//' >/pub/tjouyan/ee283/prefixes-atac.txt
+cd /pub/tjouyan/ee283
 
 file="prefixes-atac.txt"
 prefix=`head -n $SLURM_ARRAY_TASK_ID  $file | tail -n 1`
@@ -22,7 +23,7 @@ prefix=`head -n $SLURM_ARRAY_TASK_ID  $file | tail -n 1`
 module load samtools/1.15.1
 module load bwa/0.7.8
 
-bwa mem -t 2 -M $ref $input${prefix}"_R1.fq.gz" $input${prefix}"_R2.fq.gz" \
-| samtools view -bS - > $output${prefix}".bam"
-samtools sort $output${prefix}".bam" -o $output${prefix}".sort.bam"
-rm $output${prefix}".bam" 
+bwa mem -t 2 -M $ref/dm6.fasta ${input}/${prefix}_R1.fq.gz ${input}/${prefix}_R2.fq.gz \
+| samtools view -bS - > $output/${prefix}.bam
+samtools sort ${output}/${prefix}.bam -o ${output}/${prefix}.sort.bam
+#rm ${output}/${prefix}.bam
--- code/scripts/DNA-mapping.sh
+++ code/scripts/DNA-mapping.sh
@@ -2,16 +2,16 @@
 #SBATCH --job-name=map    
 #SBATCH -A class-ecoevo283    
 #SBATCH -p standard       
-#SBATCH --cpus-per-task=4  
-#SBATCH --error=map_%J.err
-#SBATCH --output=map_%J.out
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=3G    
 #SBATCH --time=1-00:00:00   
+#SBATCH --output=logs/map-dna_%A.out
+#SBATCH --error=logs/map-dna_%A.err
 #SBATCH --array=1-12
 
-input="data/raw/DNAseq/"
-output="data/processed/DNAseq/bams/"
-ref="data/ref/dm6.fasta"
+input="data/raw/DNAseq"
+qoutput="data/processed/DNAseq/bams"
+ref="data/ref"
 
 #cd $input
 #ls *1.fq.gz | sed 's/_1.fq.gz//' >~/ee283/prefixes.txt
@@ -23,7 +23,9 @@ prefix=`head -n $SLURM_ARRAY_TASK_ID  $file | tail -n 1`
 module load bwa/0.7.8
 module load samtools/1.15.1
 
-bwa mem -t 2 -M $ref ${input}${prefix}"_1.fq.gz" ${input}${prefix}"_2.fq.gz" \
-| samtools view -bS - > $output${prefix}".bam"
-samtools sort ${output}${prefix}".bam" -o ${output}${prefix}".sort.bam"
-rm $output${prefix}".bam"
+bwa mem -t 2 -M $ref/dm6.fasta ${input}/${prefix}_1.fq.gz ${input}/${prefix}_2.fq.gz \
+| samtools view -bS - > ${output}/${prefix}.bam
+samtools sort ${output}/${prefix}.bam -o ${output}/${prefix}.sort.bam 
+samtools index ${output}/${prefix}.sort.bam
+
+
