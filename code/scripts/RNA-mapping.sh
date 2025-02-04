9389fbe89bea80e36968ba1b34dc4d76cafe87d2
TaninJouyan <taninjouyan@gmail.com>
2025-02-03 21:26:25 -0800

    updated script

--- code/scripts/RNA-mapping.sh
+++ code/scripts/RNA-mapping.sh
@@ -2,27 +2,29 @@
 #SBATCH --job-name=map-rna
 #SBATCH -A class-ecoevo283
 #SBATCH -p standard
-#SBATCH --cpus-per-task=4
-#SBATCH --error=map_%J.err
-#SBATCH --output=map_%J.out
+#SBATCH --cpus-per-task=10
+#SBATCH --error=map-rna_%A.err
+#SBATCH --output=map_%A.out
 #SBATCH --mem-per-cpu=3G
 #SBATCH --time=1-00:00:00
-#SBATCH --array=1-12
+#SBATCH --array=1-20
 
 
-input="data/raw/RNAseq/"
-output="data/processed/DNAseq/bams/"
-ref="data/ref/dm6.fasta"
+input="data/raw/RNAseq"
+output="data/processed/RNAseq/bams"
+ref="data/ref/RNA-hisat"
 
-cd $input
-ls *R1.fastq.gz | sed 's/_R1.fastq.gz//' >~/ee283/prefixes-rna.txt
-cd ~/ee283
+#cd ${input}/
+#ls *R1.fastq.gz | awk '/21148|21286|22162|21297|21029|22052|22031|21293|22378|22390/ && /_[BE]_/' | sed 's/_R1.fastq.gz//' > /pub/tjouyan/ee283/prefixes-rna.txt
+#cd /pub/tjouyan/ee283/
 
-file="prefixes.txt"
+file="prefixes-rna.txt"
 prefix=`head -n $SLURM_ARRAY_TASK_ID  $file | tail -n 1`
 
-hisat2 -p 2 -x dm6_trans -1 ${input}${prefix}"_R1.fastq.gz" -2 ${input}${prefix}"_R2.fastq.gz" |\ 
-samtools view -bS - > ${input}${prefix}.bam
-samtools sort ${input}${prefix}".bam" -o ${output}${prefix}".sort.bam"
-samtools index ${output}${prefix}".sort.bam"
-rm ${input}${prefix}".bam"
+module load hisat2/2.2.1
+module load samtools/1.15.1
+
+hisat2 -x $ref/dm6_trans -1 $input/${prefix}_R1.fastq.gz -2 $input/${prefix}_R2.fastq.gz >  ${output}/${prefix}.sam 
+samtools view -bS ${output}/${prefix}.sam > ${output}/${prefix}.bam
+samtools sort ${output}/${prefix}.bam -o ${output}/${prefix}.sort.bam
+samtools index ${output}/${prefix}.sort.bam
