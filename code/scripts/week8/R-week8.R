a65eb9c7e0f4064c193712c78c66a93d45afbb4e
TaninJouyan <taninjouyan@gmail.com>
2025-03-12 08:53:34 -0700

    Update

--- code/scripts/week8/R-week8.R
+++ code/scripts/week8/R-week8.R
@@ -1,95 +1,78 @@
-install.packages('tidyverse')
+#install.packages('tidyverse')
 library(tidyverse)
 
-	
+
 setwd('/home/jovyan/pub/ee283/data')
 mal = read_tsv("allhaps.malathion.200kb.txt.gz")
-
-library(dplyr)
-library(stringr)
-library(broom)
-library(purrr)
 
 # Assuming 'mal' is your original dataset
 mal2 <- mal %>% filter(chr == "chrX" & pos == 316075)
 print(mal2)
 
+
 # Check levels of factors
 print(levels(factor(mal2$pool)))
 print(levels(factor(mal2$founder)))
 
-# Create 'treat' column
-mal2 <- mal2 %>% mutate(treat = str_sub(pool, 2, 2))
+# Create 'treat' column 
+mal2 <- mal %>% mutate(treat = str_sub(pool, 2, 2)) 
+
+print(mal2)
+library(dplyr)
+
+# Remove groups where treat or founder has only 1 unique value
+filtered_mal2<- mal2 %>%
+  group_by(chr, pos) %>%
+  filter(n_distinct(treat) > 1 & n_distinct(founder) > 1) %>%
+  ungroup()
+
+# Check if filtering worked
+print(dim(filtered_mal2))  # Should be smaller than original df
 
 # ANOVA model
 out <- anova(lm(asin(sqrt(freq)) ~ treat + founder + treat:founder, data = mal2))
 print(out)
 
-# Calculate myF
+# Calculate F distribution
 myF <- -pf(out[1, 4], out[1, 1], out[4, 1], lower.tail = FALSE, log.p = TRUE) / log(10)
 print(paste("myF:", myF))
 
 # Define mylog10pmodel function
 mylog10pmodel <- function(df) {
+  if (any(is.na(df$freq))) {
+    return(-0.5)
+  }
   out <- anova(lm(asin(sqrt(freq)) ~ treat + founder + treat:founder, data = df))
   -pf(out[1, 4], out[1, 1], out[4, 1], lower.tail = FALSE, log.p = TRUE) / log(10)
 }
 
-# Create malathion dataset
-malathion <- do.call(rbind, replicate(4, mal2, simplify = FALSE))
-malathion$chr <- rep("X", nrow(malathion))
-malathion$pos <- rep(1:4, each = nrow(mal2))
+#Define mylog10model_2 (ANOVA model absed on net effect of treatment ==0)
+mylog10pmodel_2 <- function(df) {
+    if (any(is.na(df$freq))) {
+      return(-0.5)
+    }
+    out2 <- anova(lm(asin(sqrt(freq)) ~ treat + founder + treat %in% founder, data = df))
+  -pf(out2[1, 4], out2[1, 1], out2[4, 1], lower.tail = FALSE, log.p = TRUE) / log(10)
+  }
 
-# Group and nest data
-myresult <- malathion %>%
+# Group and nest data for both models
+result <- mal2 %>%
   group_by(chr, pos) %>%
   nest() %>%
   mutate(logp = map_dbl(data, mylog10pmodel)) %>%
   select(-data)
-
-print(myresult)
-_________________________________________
-#Net effect of treatment==0
-
-# Assuming 'mal' is your original dataset
-mal2_2 <- mal %>% filter(chr == "chrX" & pos == 316075)
-print(mal2_2)
-
-# Check levels of factors
-print(levels(factor(mal2_2$pool)))
-print(levels(factor(mal2_2$founder)))
-
-# Create 'treat' column
-mal2_2 <- mal2_2 %>% mutate(treat = str_sub(pool, 2, 2))
-
-# ANOVA model with the new formula
-out_2 <- anova(lm(asin(sqrt(freq)) ~ founder + treat %in% founder, data = mal2_2))
-print(out_2)
-
-# Calculate myF_2
-myF_2 <- -pf(out_2[2, 4], out_2[2, 1], out_2[3, 1], lower.tail = FALSE, log.p = TRUE) / log(10)
-print(paste("myF_2:", myF_2))
-
-# Define mylog10pmodel_2 function
-mylog10pmodel_2 <- function(df) {
-  out <- anova(lm(asin(sqrt(freq)) ~ founder + treat %in% founder, data = df))
-  -pf(out[2, 4], out[2, 1], out[3, 1], lower.tail = FALSE, log.p = TRUE) / log(10)
-}
-
-# Create malathion_2 dataset
-malathion_2 <- do.call(rbind, replicate(4, mal2_2, simplify = FALSE))
-malathion_2$chr <- rep("X", nrow(malathion_2))
-malathion_2$pos <- rep(1:4, each = nrow(mal2_2))
-
-# Group and nest data
-myresult_2 <- malathion_2 %>%
+print(result)
+write_tsv(result,'anova_model1_result')
+result_2 <- mal2 %>%
   group_by(chr, pos) %>%
   nest() %>%
   mutate(logp = map_dbl(data, mylog10pmodel_2)) %>%
   select(-data)
 
-print(myresult_2)
+print(result_2)
+write_tsv(result_2,'anova_model2_result')
+________________________________________
 
 #Merge result dataframes
-join1= full_join(myresult, myresult_2, by=c('chr','pos'))
-join1
+join= full_join(result, result_2, by=c('chr','pos'))
+join
--- week8.md
+++ week8.md
@@ -3,8 +3,17 @@
 
 The reuslt of both models is shown in the following table. The R script related to this analysis is available [here](code/scripts/week8). 
 
-chr	pos	logp	r.squared
-X	1	0.2171851	0.9231759
-X	2	0.2171851	0.9231759
-X	3	0.2171851	0.9231759
-X	4	0.2171851	0.9231759
+| Index | Chromosome | Position | logp.x     | logp.y     |
+|-------|-----------|----------|------------|------------|
+| 1     | chrX      | 316075   | 0.21718506 | 0.22209860 |
+| 2     | chrX      | 336075   | 0.33603094 | 0.34591865 |
+| 3     | chrX      | 356075   | 0.41239107 | 0.42662523 |
+| 4     | chrX      | 376075   | 0.38940081 | 0.40222365 |
+| 5     | chrX      | 396075   | 0.36248061 | 0.37376460 |
+| 6     | chrX      | 416075   | 0.12130618 | 0.12353085 |
+| 7     | chrX      | 436075   | 0.09138484 | 0.09296253 |
+| 8     | chrX      | 456075   | 0.09658811 | 0.09827273 |
+| 9     | chrX      | 476075   | 0.11798080 | 0.12012961 |
+| 10    | chrX      | 496075   | 0.14175556 | 0.14447017 |
+
+logp.x corresponds to the ANOVA model whre the net effect of the treament equals to zero. And logp.y is calculated with the ANOVA model where there is a two-way interaction between treat and founder. 
